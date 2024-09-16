fed14a7af4e3a152e69d6e38fb23a6fadc1df895
Rohankhan999 <rohankhan08453@gmail.com>
2024-09-17 00:00:53 +0500

    Milestone3

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // Get refrence to the form and display area//
-var from = document.getElementById('resume-form');
+var form = document.getElementById('resume-form');
 var resumeDisplayelement = document.getElementById('resume-display');
 // handle form submission//
-from.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault(); //prevent page reload
     //collect input values//
     var name = document.getElementById('Name').value;
     var fathername = document.getElementById('fathername').value;
-    var phno = document.getElementById('phno').value;
+    var Phone = document.getElementById('phno').value;
     var email = document.getElementById('email').value;
     var Address = document.getElementById('Address').value;
     var Education = document.getElementById('Education').value;
     var Skills = document.getElementById('Skills').value;
-    var WorkExperience = document.getElementById(' WorkExperience ').value;
+    var WorkExperience = document.getElementById('WorkExperience').value;
     //Generate the resume content dynamicaly//
-    var resumeHTML = "\n<h2><b>Resume</b></h2>\n<h3>Personal Informaion</h3>\n<p><b>Name:</b>".concat(name, "</p>\n<p><b>fathername:</b>").concat(fathername, "</p>\n<p><b>Phone number:</b>").concat(phno, "</p>\n<p><b>Email:</b>").concat(email, "</p>\n<p><b>Address:</b>").concat(Address, "</p>\n\n<h3>Education</h3>\n<p>").concat(Education, "</p>\n\n<h3>Skills</h3>\n<p>").concat(Skills, "</p>\n\n<h3>Work Experience</h3>\n<p>").concat(WorkExperience, "</p>\n");
+    var resumeoutput = "\n<h2><b>Resume</b></h2>\n<h3>Personal Informaion</h3>\n<p><b>Name:</b> ".concat(name, "</p>\n<p><b>fathername:</b> ").concat(fathername, "</p>\n<p><b>Phone number:</b> ").concat(Phone, "</p>\n<p><b>Email:</b> ").concat(email, "</p>\n<p><b>Address:</b> ").concat(Address, "</p>\n\n<h3>Education</h3>\n<p>").concat(Education, "</p>\n\n<h3>Skills</h3>\n<p>").concat(Skills, "</p>\n\n<h3>Work Experience</h3>\n<p>").concat(WorkExperience, "</p>\n");
     if (resumeDisplayelement) {
-        resumeDisplayelement.innerHTML=resumeHTML;
+        resumeDisplayelement.innerHTML = resumeoutput;
     }
     else {
         console.error('The Resume Display Element is Missing!');
--- app.ts
+++ app.ts
@@ -1,32 +1,32 @@
 // Get refrence to the form and display area//
-const form = document.getElementById('resume-form') as HTMLFormElement;
-const resumeDisplayelement =document.getElementById('resume-display') as HTMLDivElement;
+const form = document.getElementById('resume-form');
+const resumeDisplayelement = document.getElementById('resume-display');
 
 
 // handle form submission//
-form.addEventListener("submit", (event:Event)=>{
+form.addEventListener("submit", (event) => {
     event.preventDefault();//prevent page reload
 
     //collect input values//
-    const name =(document.getElementById('Name') as HTMLInputElement).value;
-    const fathername =(document.getElementById('fathername') as HTMLInputElement).value
-    const phno =(document.getElementById('phno') as HTMLInputElement).value
-    const email =(document.getElementById('email') as HTMLInputElement).value
-    const Address  =(document.getElementById('Address') as HTMLInputElement).value
-    const Education = (document.getElementById('Education') as HTMLInputElement).value
-    const Skills = (document.getElementById('Skills') as HTMLInputElement).value
-    const WorkExperience = (document.getElementById(' WorkExperience ') as HTMLInputElement).value
+    const name = (document.getElementById('Name') as HTMLInputElement).value;
+    const fathername = (document.getElementById('fathername') as HTMLInputElement).value
+    const Phone = (document.getElementById('phno') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const Address = (document.getElementById('Address') as HTMLInputElement).value
+    const Education = (document.getElementById('Education') as HTMLTextAreaElement).value
+    const Skills = (document.getElementById('Skills') as HTMLTextAreaElement).value
+    const WorkExperience = (document.getElementById('WorkExperience') as HTMLTextAreaElement).value
 
 
 //Generate the resume content dynamicaly//
-const resumeHTML= `
+const resumeoutput = `
 <h2><b>Resume</b></h2>
 <h3>Personal Informaion</h3>
-<p><b>Name:</b>${name}</p>
-<p><b>fathername:</b>${fathername}</p>
-<p><b>Phone number:</b>${phno}</p>
-<p><b>Email:</b>${email}</p>
-<p><b>Address:</b>${Address}</p>
+<p><b>Name:</b> ${name}</p>
+<p><b>fathername:</b> ${fathername}</p>
+<p><b>Phone number:</b> ${Phone}</p>
+<p><b>Email:</b> ${email}</p>
+<p><b>Address:</b> ${Address}</p>
 
 <h3>Education</h3>
 <p>${Education}</p>
@@ -37,10 +37,13 @@ const resumeHTML= `
 <h3>Work Experience</h3>
 <p>${WorkExperience}</p>
 `;
-if(resumeDisplayelement){   
-    resumeDisplayelement.innerHTML = resumeHTML;
+
+if (resumeDisplayelement) {   
+    resumeDisplayelement.innerHTML = resumeoutput;
 }else{
     console.error('The Resume Display Element is Missing!');
 }
+
+});
   
-})
+
